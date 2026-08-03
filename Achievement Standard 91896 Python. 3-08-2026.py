adbef1057fc096538a742fc691220f1d5222422c
ElliotGuyan <st23271@howick.school.nz>
2026-08-04 10:49:12 +1200

    Add files via upload
    Description of database purpose and personal notes.
    Global variable containing passwords to enter the programme. Maintaining the security of the citizens in the database.
    Start of login confirmation; no real restrictions as of right now.

---
+++ Achievement Standard 91896 Python. 3-08-2026.py
@@ -0,0 +1,36 @@
+'''
+Patrol infringement management programme.
+
+The purpose of this programme is to support patrol officers in their work. 
+This is done by giving the user inputting the information from the driver who commited a traffick violation.
+The programme will then return a ticket for the violation, and store the data for later use.
+This programme has multiple different uses, it can record speeding offences, view offences, search offences, and display the patrol summary.
+
+
+Elliot Guyan
+Started - 03/08/2026
+Achievement standard 91896
+'''
+
+'''
+Things do to, ideas, and trialing.
+
+Navigation:
+- Record speeding offence
+- View all recorded offences
+- Search offence records
+- Display patrol summary
+
+Record a speeding offence
+- Driver's full name: Must be at least 2 names, exclude numbers, and all characters except 'space' '
+- Driver's license number: Two capatalised letters followed by 6 numbers eg (AB123456)
+- Posted speed limit: increments of 10 from 30-110
+- Driver's recorded speed: Any number above 31, must be greater than the speed limit
+
+Password
+- Add a password for information security. Ask for badge number and password.
+
+
+'''
+
+
