0b0f8f2e9fd658f105e5c7dc3345f4f3c777ea93
ElliotGuyan <st23271@howick.school.nz>
2026-08-07 12:21:56 +1200

    Add files via upload
    Finished the second welcome message. For when the user enters the programme by entering a valid password
    
    Added user_choice, allowing the user to enter what they would like to do. Incomplete functions/connections and names.

---
+++ Achievement Standard 91896 Python. 7-08-2026.py
@@ -0,0 +1,107 @@
+'''
+Patrol infringement management programme.
+
+The purpose of this programme is to support patrol officers in their work. 
+This is done by giving the user inputting the information from the driver who committed a traffic violation.
+The programme will then return a ticket for the violation, and store the data for later use.
+This programme has multiple different uses; it can record speeding offences, view offences, search offences, and display the patrol summary.
+
+
+Elliot Guyan
+Started - 03/08/2026
+Achievement standard 91896
+'''
+
+'''
+Things to do, ideas, and trialling.
+
+Navigation:
+- Record speeding offence
+- View all recorded offences
+- Search offence records
+- Display patrol summary
+
+Record a speeding offence
+- Driver's full name: Must be at least 2 names, exclude numbers, and all characters except 'space' '
+- Driver's license number: Two capitalised letters followed by 6 numbers eg (AB123456)
+- Posted speed limit: increments of 10 from 30-110
+- Driver's recorded speed: Any number above 31 must be greater than the speed limit
+
+Password
+- Add a password for information security. Ask for badge number and password.
+- Add a number of attempts before exiting programme.
+
+
+'''
+
+'''Storing the password of the police officer, enforcing them to enter the programme
+via passwords, increasing user safety and the privacy of citizens.'''
+passwords = {
+    "pin": "12345",
+    "badge_number": "LI123"
+    } 
+
+
+
+
+def display_login_message():
+    '''Displays the first of the of the welcome messages.
+    This one asks the user to enter their badge number and password 
+    before entering the programme. This allows for citizen privacy, as 
+    information can be found out about them through this programme.'''
+
+    print("Welcome to the patrol infringement management programme. \
+          To enter, please enter your badge number and pin.")
+
+def login_confirmation(passwords):
+    '''Checks if the user's password and badge number are real, 
+    allowing the safety of citizen information and privacy.'''
+
+    user = input("Please enter your badge number: ")
+    pin = input("Please enter your pin: ")
+    if user == passwords["badge_number"] and pin == passwords["pin"]:
+        return True
+    else:
+        return False 
+
+
+#---------------------------- Main Programme -------------------------------#
+
+if login_confirmation(passwords) == False:
+    '''Checks if the login which the user has given is valid or not.
+    If the user login is not valid, it will reject them and close the programme. 
+    If the user login is valid it will continue on into the programme.'''
+
+    print("The badge number or password entered is incorrect, access denied.")
+else: 
+    print("Welcome to the Patrol Infringement Management Programme.\n\
+This programme...")
+    
+    print('''
+    1: Record a speeding offence
+    2: View all recorded speeding offences
+    3: Search offence records
+    4: Display patrol summary
+    5: Exit programme
+''')
+    
+user_choice = input("Please enter the number corresponding to your requirements: ")
+'''Allows the user to decide what they want to do. Whether it is to record a
+speed offence, search the records, or display the patrol summary.'''
+
+while user_choice != "5":
+    if user_choice == "1":
+        print("")
+        #recorded_offence = input("Please enter the licence number of the offender: ")
+    elif user_choice == "2":
+        print("")
+    elif user_choice == "3":
+        print("")
+    elif user_choice == "4":
+        print()
+
+    else:
+        print("Please enter a valid option between 1 and 5.")
+    
+        user_choice = input("Please enter the number corresponding to your requirements: ")
+
